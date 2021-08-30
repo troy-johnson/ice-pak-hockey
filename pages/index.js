0b8b6ee84c9e9a109b1516afab45c226dbedabb4
troy.johnson <troy.johnson57@gmail.com>
2021-08-29 22:48:10 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Home = () => {
+   return (
+      <>
+         <Head>
+            <title>Ice Pak Hockey</title>
+            <meta name="description" content="Ice Pak Hockey" />
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+
+         <main>
+            <h1>Welcome to Ice Pak Hockey!</h1>
+         </main>
+      </>
+   );
+};
+
+export default Home;
---
+++ pages/roster.js
@@ -0,0 +1,5 @@
+const Roster = () => {
+   return <h1>Roster</h1>;
+};
+
+export default Roster;
