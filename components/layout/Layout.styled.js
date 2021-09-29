12b071134dfd514351fe719c229bdb28bc56403f
Troy Johnson <30636163+troy-johnson@users.noreply.github.com>
2021-09-29 08:33:00 -0600

    Delete Layout.styled.js

--- components/layout/Layout.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const StyledMain = styled.main`
-left: 1.5rem;
-
-@media (max-width: ${({ theme }) => theme.mobile}) {
-   margin: 1rem 0.25rem;
-}
-`;
