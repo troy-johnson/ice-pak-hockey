7f80339c19984b7cc0d19561dcfa69d210281741
Troy Johnson <30636163+troy-johnson@users.noreply.github.com>
2021-09-29 08:32:46 -0600

    Create Layout.styled.js

---
+++ components/Layout/Layout.styled.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const StyledMain = styled.main`
+  left: 1.5rem;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+     margin: 1rem 0.25rem;
+  }
+`;
