e38d0f0a9f71b92bc909f4384c2ae0f134fe9f76
troy.johnson <troy.johnson57@gmail.com>
2021-11-04 17:20:42 -0600

    added game day notifications

--- pages/api/notifications/game-day.js
+++ pages/api/notifications/game-day.js
@@ -1,46 +1,118 @@
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, documentId, query, where } from "firebase/firestore";
 import dayjs from "dayjs";
+import twilio from "twilio";
 import { db } from "../../../config";
 
 const gameDayNotificationHandler = async (req, res) => {
    switch (req.method) {
       case "POST":
-         const gameResult = await getDocs(collection(db, "games"));
-         const playerResult = await getDocs(collection(db, "players"));
+         try {
+            let games = [];
+            let players = [];
+            let locations = [];
+            let opponents = [];
 
-         let games = [];
-         let players = [];
+            const gameResult = await getDocs(collection(db, "games"));
+            const playerResult = await getDocs(
+               query(collection(db, "players"), where("notifications.gameDay", "==", true))
+            );
 
-         gameResult.forEach((doc) => {
-            games.push({ id: doc.id, ...doc.data() });
-         });
+            gameResult.forEach((doc) => {
+               games.push({ id: doc.id, ...doc.data() });
+            });
 
-         playerResult.forEach((doc) => {
-            players.push({ id: doc.id, ...doc.data() });
-         });
+            playerResult?.forEach((doc) => {
+               players.push({
+                  id: doc.id,
+                  firstName: doc.data().firstName,
+                  lastName: doc.data().lastName,
+                  phoneNumber: doc.data().phoneNumber,
+                  preferredPhone: doc.data()?.preferredPhone,
+                  preferredEmail: doc.data()?.preferredEmail,
+               });
+            });
 
-         const gameDay = games.map(
-            (game) => dayjs.unix(game.date.seconds).diff(dayjs(), "hour")
-         );
+            const gameDay = games
+               .map((game) => {
+                  return {
+                     ...game,
+                     difference: dayjs.unix(game.date.seconds).diff(dayjs(), "hour"),
+                  };
+               })
+               .filter((game) => game.difference <= 12 && game.difference >= 0)
+               .sort((a, b) => a.difference - b.difference)[0];
 
-         console.log("gameDay", gameDay)
+            if (!gameDay) {
+               return res.status(200).json({ message: "Not a game day." });
+            }
 
-         return res.status(200).json(gameDay);
+            const opponentResult = await getDocs(
+               query(collection(db, "opponents"), where(documentId(), "==", gameDay.opponentId))
+            );
+            const locationResult = await getDocs(
+               query(collection(db, "locations"), where(documentId(), "==", gameDay.locationId))
+            );
 
-      // try {
-      //    const result = await getDoc(doc(db, "penalties", id));
+            opponentResult.forEach((opponent) =>
+               opponents.push({ id: opponent.id, ...opponent.data() })
+            );
 
-      //    const penalty = { ...result.data() };
+            locationResult.forEach((location) =>
+               locations.push({ id: location.id, ...location.data() })
+            );
 
-      //    if (penalty) {
-      //       return res.status(200).json(penalty);
-      //    }
+            const opponentInfo = opponents.filter(
+               (opponent) => opponent.id === gameDay.opponentId
+            )[0];
+            const locationInfo = locations.filter(
+               (location) => location.id === gameDay.locationId
+            )[0];
 
-      //    return res.status(200).send("Penalty not found!");
-      // } catch (error) {
-      //    console.log("error", error);
-      //    return res.status(400).send(error);
-      // }
+            console.log("gameDay", { players, gameDay, opponentInfo, locationInfo });
+
+            if (gameDay) {
+               let listToNotify = players?.filter((player) => gameDay.roster.includes(player.id));
+
+               const textClient = twilio(
+                  process.env.TWILIO_ACCOUNT_SID,
+                  process.env.TWILIO_AUTH_TOKEN
+               );
+
+               listToNotify.forEach((player) => {
+                  textClient.messages
+                     .create({
+                        from: "(714) 519-2916",
+                        to: player.preferredPhone ?? player.phoneNumber,
+                        body: `Ice Pak Hockey -- It's game day! \n\nOpponent: ${
+                           opponentInfo.teamName
+                        } \nDate and Time: ${dayjs
+                           .unix(gameDay.date.seconds)
+                           .format("MMM D @ h:m")} \nLocation: ${locationInfo.name} (${
+                           locationInfo.googleMapsLink
+                        }) \n \nView game at www.icepakhockey.com/games/${gameDay.id}`,
+                     })
+                     .then((res) =>
+                        console.log(
+                           `successfully sent game day sms notification to ${player.firstName} ${player.lastName}`,
+                           res
+                        )
+                     )
+                     .catch((err) =>
+                        console.log(
+                           `error sending message to ${player.firstName} ${player.lastName}`,
+                           err
+                        )
+                     );
+               });
+
+               return res
+                  .status(200)
+                  .json({ message: "Successfully sent game day text notifications.", gameDay });
+            }
+         } catch (error) {
+            console.log("error", error);
+            return res.status(400).json({ message: error });
+         }
       default:
          break;
    }
