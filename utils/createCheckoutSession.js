117c7f5c72c52e97f04bd0c1846546efcf78c52c
troy.johnson <troy.johnson57@gmail.com>
2021-12-02 16:25:52 -0700

    updated all api routes to allow cors

--- utils/createCheckoutSession.js
+++ utils/createCheckoutSession.js
@@ -3,30 +3,23 @@ const createCheckoutSession = async (data) => {
 
    const fullName = `${data?.user?.firstName} ${data?.user?.lastName}`;
 
-   const response = await fetch(
-      `${
-         process.env.NODE_ENV === "development"
-            ? "http://localhost:3000"
-            : "https://www.icepakhockey.com"
-      }/api/checkout/`,
-      {
-         mode: "cors",
-         cache: "no-cache",
-         headers: {
-            "Content-Type": "application/json",
+   const response = await fetch("/api/checkout/", {
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+         items: data.cart,
+         user: {
+            email: data?.user?.email,
+            firstName: data?.user?.firstname,
+            lastName: data?.user?.lastName,
+            fullName,
          },
-         method: "POST",
-         body: JSON.stringify({
-            items: data.cart,
-            user: {
-               email: data?.user?.email,
-               firstName: data?.user?.firstname,
-               lastName: data?.user?.lastName,
-               fullName,
-            },
-         }),
-      }
-   );
+      }),
+   });
 
    return response.json();
 };
