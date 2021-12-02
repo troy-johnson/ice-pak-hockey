cb69a4852321afb3cc6f37aed4cfb7971558be3d
troy.johnson <troy.johnson57@gmail.com>
2021-12-02 16:39:45 -0700

    updated error messaging for webhooks

--- pages/api/webhooks/printful.js
+++ pages/api/webhooks/printful.js
@@ -5,25 +5,29 @@ import twilio from "twilio";
 const textClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 const printfulWebhookHandler = async (req, res) => {
-   const { type, data } = req.body;
+   try {
+      const { type, data } = req.body;
 
-   const orderResult = await getDoc(doc(db, "orders", data.order.external_id));
+      const orderResult = await getDoc(doc(db, "orders", data.order.external_id));
 
-   const orderData = orderResult.data();
+      const orderData = orderResult.data();
 
-   if (type === "package_shipped") {
-      await updateDoc(doc(db, "orders", data.order.external_id), {
-         shippingStatus: "Package shipped",
-         status: "Package shipped",
-         shippingData: data.shipment,
-         packingSlip: data.order.packing_slip,
-      });
+      if (type === "package_shipped") {
+         await updateDoc(doc(db, "orders", data.order.external_id), {
+            shippingStatus: "Package shipped",
+            status: "Package shipped",
+            shippingData: data.shipment,
+            packingSlip: data.order.packing_slip,
+         });
 
-      await textClient.messages.create({
-         from: "(714) 519-2916",
-         to: orderData.contact.phone,
-         body: `Ice Pak Hockey \n\n Order #${data.order.external_id} has shipped! \n\n Track your shipment: ${data.shipment.tracking_url} \n\n View your order: https://icepakhockey.com/order-status/${data.order.external_id} \n\n Thanks for shopping at the Ice Pak Hockey store! \n\n Need help? Text or email Troy at (801) 913-4614 or troy.johnson57@gmail.com `,
-      });
+         await textClient.messages.create({
+            from: "(714) 519-2916",
+            to: orderData.contact.phone,
+            body: `Ice Pak Hockey \n\n Order #${data.order.external_id} has shipped! \n\n Track your shipment: ${data.shipment.tracking_url} \n\n View your order: https://icepakhockey.com/order-status/${data.order.external_id} \n\n Thanks for shopping at the Ice Pak Hockey store! \n\n Need help? Text or email Troy at (801) 913-4614 or troy.johnson57@gmail.com `,
+         });
+      }
+   } catch (error) {
+      return res.status(400).send({ status: 400, message: error });
    }
 };
 
--- pages/api/webhooks/stripe.js
+++ pages/api/webhooks/stripe.js
@@ -20,82 +20,87 @@ const stripeWebhookHandler = async (req, res) => {
    const buf = await buffer(req);
    const sig = req.headers["stripe-signature"];
 
-   let event;
-
    try {
-      event = stripe.webhooks.constructEvent(buf.toString(), sig, endpointSecret);
-   } catch (err) {
-      res.status(400).send(`Webhook Error: ${err.message}`);
+      let event;
+
+      try {
+         event = stripe.webhooks.constructEvent(buf.toString(), sig, endpointSecret);
+      } catch (err) {
+         res.status(400).send(`Webhook Error: ${err.message}`);
+      }
+
+      const dataObject = event.data.object;
+
+      const { client_reference_id, customer_details, payment_status, shipping, status } =
+         dataObject;
+
+      if (event.type === "checkout.session.completed") {
+         console.log(`💰 PaymentIntent status: ${dataObject.status}`);
+
+         console.log("paymentIntent", event.data);
+
+         if (client_reference_id) {
+            await updateDoc(doc(db, "orders", client_reference_id), {
+               stripeData: { ...event.data.object },
+               contact: { ...customer_details },
+               orderStatus: "Pending order confirmation",
+               paymentStatus: payment_status,
+               shippingStatus: "Pending order confirmation",
+               status: "Pending order confirmation",
+               shipping,
+            });
+
+            const submitOrderResult = await fetch(
+               `${
+                  process.env.NODE_ENV === "development"
+                     ? "http://localhost:3000"
+                     : "https://icepakhockey.com"
+               }/api/orders/${client_reference_id}`,
+               { method: "POST" }
+            );
+
+            return res.status(200).send({ message: "Payment success! Submitting order..." });
+         }
+      } else if (event.type === "payment_intent.payment_failed") {
+         console.log(`❌ Payment failed: ${dataObject.last_payment_error?.message}`);
+
+         if (client_reference_id) {
+            await updateDoc(doc(db, "orders", client_reference_id), {
+               stripeData: { ...event.data.object },
+               paymentStatus: payment_status,
+               status,
+            });
+
+            return res.status(202).send("Updated order status.");
+         }
+      } else if (event.type === "charge.succeeded") {
+         if (dataObject.payment_intent) {
+            const paymentIntent = await stripe.paymentIntents.retrieve(dataObject.payment_intent);
+
+            const session = await stripe.checkout.sessions.list({
+               payment_intent: dataObject.payment_intent,
+            });
+
+            await updateDoc(doc(db, "orders", session.data[0].client_reference_id), {
+               receiptUrl: paymentIntent.data.receipt_url,
+            });
+         }
+      } else {
+         console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+         if (client_reference_id) {
+            await updateDoc(doc(db, "orders", client_reference_id), {
+               stripeData: { ...event.data.object },
+               status,
+            });
+
+            return res.status(202).send("Updated order status.");
+         }
+      }
+
+      res.json({ received: true });
+   } catch (error) {
+      return res.status(400).send({ status: 400, message: error });
    }
-
-   const dataObject = event.data.object;
-
-   const { client_reference_id, customer_details, payment_status, shipping, status } = dataObject;
-
-   if (event.type === "checkout.session.completed") {
-      console.log(`💰 PaymentIntent status: ${dataObject.status}`);
-
-      console.log("paymentIntent", event.data);
-
-      if (client_reference_id) {
-         await updateDoc(doc(db, "orders", client_reference_id), {
-            stripeData: { ...event.data.object },
-            contact: { ...customer_details },
-            orderStatus: "Pending order confirmation",
-            paymentStatus: payment_status,
-            shippingStatus: "Pending order confirmation",
-            status: "Pending order confirmation",
-            shipping,
-         });
-
-         const submitOrderResult = await fetch(
-            `${
-               process.env.NODE_ENV === "development"
-                  ? "http://localhost:3000"
-                  : "https://icepakhockey.com"
-            }/api/orders/${client_reference_id}`,
-            { method: "POST" }
-         );
-
-         return res.status(200).send({ message: "Payment success! Submitting order..." });
-      }
-   } else if (event.type === "payment_intent.payment_failed") {
-      console.log(`❌ Payment failed: ${dataObject.last_payment_error?.message}`);
-
-      if (client_reference_id) {
-         await updateDoc(doc(db, "orders", client_reference_id), {
-            stripeData: { ...event.data.object },
-            paymentStatus: payment_status,
-            status,
-         });
-
-         return res.status(202).send("Updated order status.");
-      }
-   } else if (event.type === "charge.succeeded") {
-      if (dataObject.payment_intent) {
-         const paymentIntent = await stripe.paymentIntents.retrieve(dataObject.payment_intent);
-
-         const session = await stripe.checkout.sessions.list({
-            payment_intent: dataObject.payment_intent,
-         });
-
-         await updateDoc(doc(db, "orders", session.data[0].client_reference_id), {
-            receiptUrl: paymentIntent.data.receipt_url,
-         });
-      }
-   } else {
-      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
-      if (client_reference_id) {
-         await updateDoc(doc(db, "orders", client_reference_id), {
-            stripeData: { ...event.data.object },
-            status,
-         });
-
-         return res.status(202).send("Updated order status.");
-      }
-   }
-
-   res.json({ received: true });
 };
 
 export default stripeWebhookHandler;
