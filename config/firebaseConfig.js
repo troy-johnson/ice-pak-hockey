a81e92a40aebb594e3ebb3585e84acf369566f29
troy.johnson <troy.johnson57@gmail.com>
2021-12-09 12:10:10 -0700

    updated firebase config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,14 +1,21 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-if (getApps().length === 0) {
-   initializeApp({
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-   });
-}
+const app = initializeApp({
+   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+});
+const db = getFirestore(app);
 
-const db = getFirestore();
+// if (getApps().length === 0) {
+//    initializeApp({
+//       apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//       authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//    });
+// }
+
+// const db = getFirestore();
 
 export default db;
